cd8de567e1176a83cf39942a8454d8e523b40a2d
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-05-11 10:16:40 -0700

    Add basic flexbox and center/zoom map

--- src/map.ts
+++ src/map.ts
@@ -5,11 +5,7 @@ mapboxgl.accessToken =
 
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/kengreim/clw1n6q6s01zr01q1evxbepei", // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-  center: [41, 21], // starting position
-  zoom: 3, // starting zoom
-});
-
-map.on("load", (e) => {
-  console.log(map.getStyle());
+  style: "mapbox://styles/kengreim/clw1n6q6s01zr01q1evxbepei",
+  center: [-121.4, 37.8], // starting position to center NCT visually
+  zoom: 7, // starting zoom
 });
