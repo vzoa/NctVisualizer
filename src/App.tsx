de3574c132ef02ceb8425d45c87562c42f033407
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-05-11 21:15:19 -0700

    Add map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./index.css";
+import Map from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 export function App() {
   return (
@@ -8,7 +10,17 @@ export function App() {
         <h1 className="text-white text-2xl">NCT Visualizer</h1>
       </div>
       <div className="grow">
-        <div id="map" className="w-full h-full"></div>
+        <div id="map" className="w-full h-full">
+          <Map
+            mapboxAccessToken="pk.eyJ1Ijoia2VuZ3JlaW0iLCJhIjoiY2x3MW15aGtzMGRuNzJrbHIybWNuM3BibyJ9.aMwzzc4Q0-bPHtWI-_MEQg"
+            initialViewState={{
+              latitude: 37.8,
+              longitude: -121.4,
+              zoom: 7.2,
+            }}
+            mapStyle="mapbox://styles/kengreim/clw1n6q6s01zr01q1evxbepei"
+          />
+        </div>
       </div>
     </div>
   );
