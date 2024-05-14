9f58e229c83a9fecf49e776c27c5d814c7927c86
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-05-13 20:10:29 -0700

    Add prettier

--- src/MapStyleSelector.tsx
+++ src/MapStyleSelector.tsx
@@ -8,15 +8,24 @@ interface MapStyle {
   disabled: boolean;
 }
 
-export const DEFAULT_MAP_STYLE: MapStyle = { value: "mapbox://styles/mapbox/empty-v9", label: "Empty", disabled: false }
+export const DEFAULT_MAP_STYLE: MapStyle = {
+  value: "mapbox://styles/mapbox/empty-v9",
+  label: "Empty",
+  disabled: false,
+};
 
-const MAP_STYLES: MapStyle[] = [DEFAULT_MAP_STYLE].concat([
-  { value: "mapbox://styles/mapbox/light-v11", label: "World Light", disabled: false }
-]);
+const MAP_STYLES: MapStyle[] = [
+  DEFAULT_MAP_STYLE,
+  {
+    value: "mapbox://styles/mapbox/light-v11",
+    label: "World Light",
+    disabled: false,
+  },
+];
 
 interface MapStyleSelectorProps {
-  style: Accessor<MapStyle>,
-  setStyle: Setter<MapStyle>
+  style: Accessor<MapStyle>;
+  setStyle: Setter<MapStyle>;
 }
 
 const MapStyleSelector: Component<MapStyleSelectorProps> = (props) => {
@@ -28,14 +37,16 @@ const MapStyleSelector: Component<MapStyleSelectorProps> = (props) => {
       optionDisabled="disabled"
       value={props.style()}
       onChange={props.setStyle}
-      itemComponent={(props) => <SelectItem item={props.item}>{props.item.rawValue.label}</SelectItem>}
+      itemComponent={(props) => (
+        <SelectItem item={props.item}>{props.item.rawValue.label}</SelectItem>
+      )}
     >
       <SelectTrigger aria-label="Map Style" class="w-[180px]">
         <SelectValue<MapStyle>>{(state) => state.selectedOption().label}</SelectValue>
       </SelectTrigger>
-      <SelectContent/>
+      <SelectContent />
     </Select>
   );
-}
+};
 
-export { MapStyleSelector }
+export { MapStyleSelector };
