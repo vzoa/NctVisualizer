e426bf753001eab78438cfe9d6b8ae0196155e9f
Ken Greim <34892440+kengreim@users.noreply.github.com>
2024-05-13 23:32:44 -0700

    Fix style empty selection bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,12 @@ import type { Config } from "tailwindcss";
 import KobalteTailwindPlugin from "@kobalte/tailwindcss";
 
 const config: Config = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}"],
   darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [KobalteTailwindPlugin, require("tailwindcss-animate")],
+  plugins: [KobalteTailwindPlugin],
 };
 
 export default config;
