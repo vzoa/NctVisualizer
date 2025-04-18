717e171693d2fc8b8f8716bb67027553de68e9de
Ken Greim <34892440+kengreim@users.noreply.github.com>
2025-04-17 17:23:36 -0700

    Fix overscroll and checkbox screwing up scrolling

--- src/components/ui-core/Checkbox.tsx
+++ src/components/ui-core/Checkbox.tsx
@@ -16,7 +16,10 @@ type PolymorphicStyledCheckboxProps<T extends ValidComponent = "div"> = Polymorp
 const Checkbox: Component<PolymorphicStyledCheckboxProps> = (props) => {
   const [, rest] = splitProps(props, ["class"]);
   return (
-    <CheckboxPrimitive class={cn("items-center group flex space-x-2", props.class)} {...rest}>
+    <CheckboxPrimitive
+      class={cn("items-center relative group flex space-x-2", props.class)}
+      {...rest}
+    >
       <CheckboxPrimitive.Input />
       <CheckboxPrimitive.Control class="h-[20px] w-[20px] rounded-sm border border-gray-200 bg-slate-800 hover:bg-slate-700 transition-colors data-checked:bg-orange-700 data-checked:hover:bg-orange-700 data-checked:text-white">
         <CheckboxPrimitive.Indicator>
